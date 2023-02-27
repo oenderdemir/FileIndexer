39be4618b28b6988c253102fe1e1c1440eb26d2c
oender.demir <oender.demir@gmail.com>
2023-02-27 23:44:26 +0300

    doc ve pdf file eklendi.

---
+++ FileIndexer/FileIndexer/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.ExtendedProperties;
+
+
+Console.WriteLine("Hello, World!");
+IndeksDOC(@"C:\Autopsy\2023_02\Export\İŞE İZİNSİZ GELMEME.docx");
+
+void IndeksPDF(string fileName)
+{
+    using (PdfDocument document = PdfDocument.Open(fileName))
+    {
+        foreach (Page page in document.GetPages())
+        {
+            string pageText = page.Text;
+
+            foreach (Word word in page.GetWords())
+            {
+                Console.WriteLine(word.Text);
+            }
+        }
+    }
+}
+
+void IndeksDOC(string fileName)
+{
+
+
+    using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(fileName, true))
+    {
+        Body body = wordDoc.MainDocumentPart.Document.Body;
+        string contents = "";
+
+        foreach (Paragraph co in
+                    wordDoc.MainDocumentPart.Document.Body.Descendants<Paragraph>())
+        {
+            contents += co.InnerText;
+        }
+      
+        Console.WriteLine(contents);
+    }
+}
